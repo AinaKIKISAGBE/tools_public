ba908f1fa25e1b5fbd3d927c1ae4479c8e88fe34
Amiral KIKI-SAGBE <aina.kiki.sagbe@gmail.com>
2023-04-05 21:44:24 +0200

    add ....5230

--- tools/service_creator_docker.sh
+++ tools/service_creator_docker.sh
@@ -1,11 +1,11 @@
-service_create=$1
-link_service_create=$2
+service_create="master_update_iptable" $1
+link_service_create="/myapp/tools_private/vm_provitionning/master/master_update_iptable.sh"  $2
 
 #service_create=worker_update_iptable
 #link_service_create=/myapp/tools_private/vm_provitionning/worker/${service_create}.sh
 
 killall ${service_create} 
-systemctl stop ${service_create}.service 
+service ${service_create}.service stop 
 rm /etc/systemd/system/multi-user.target.wants/${service_create}.service 
 rm /usr/local/bin/${service_create}.sh 
 touch /usr/local/bin/${service_create}.sh 
@@ -20,12 +20,29 @@ echo "[Service] " >> /etc/systemd/system/${service_create}.service
 echo "Type=oneshot " >> /etc/systemd/system/${service_create}.service 
 echo "RemainAfterExit=yes " >> /etc/systemd/system/${service_create}.service 
 echo "ExecStart=/bin/bash /usr/local/bin/${service_create}.sh start  " >> /etc/systemd/system/${service_create}.service 
-echo "RestartSec=1s " >> /etc/systemd/system/${service_create}.service 
 echo "[Install] " >> /etc/systemd/system/${service_create}.service 
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/${service_create}.service 
 chmod 777 /etc/systemd/system/${service_create}.service 
 rm /etc/systemd/system/multi-user.target.wants/${service_create}.service 
 ln -s /lib/systemd/system/${service_create}.service /etc/systemd/system/multi-user.target.wants/ 
-systemctl daemon-reload 
-systemctl enable ${service_create}.service  
-systemctl start ${service_create}.service
+service daemon-reload 
+service  ${service_create}.service  enable
+service  ${service_create}.service start
+
+
+/etc/systemd/system/daemon.service
+rm /etc/systemd/system/${service_create}.service 
+touch /etc/systemd/system/${service_create}.service 
+
+
+[Unit]
+Description=Your Daemon Name
+
+[Service]
+ExecStart=/path/to/executable
+Restart=on-failure
+RestartSec=1s
+
+[Install]
+WantedBy=multi-user.target
+
