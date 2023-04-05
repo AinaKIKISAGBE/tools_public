7d26236af5c54e7f3ca00d868e3b3fd3897419c5
Amiral KIKI-SAGBE <aina.kiki.sagbe@gmail.com>
2023-04-05 23:03:35 +0200

    add ....523056

---
+++ tools/Dockerfile_provision_master.sh
@@ -0,0 +1,117 @@
+
+### cmd1 && cmd2 : vérifie si la première commande s'est bien exécuter avant d'exécuter la suivante sinon le processus s'arrete 
+
+### cmd1 & cmd2 : exécute les commandes sans vérifier si la commande précédente s'est bien exécuté ou pas
+
+
+
+apt-get update  
+apt-get install sudo -y
+
+
+
+################### add user and set password  ####################
+# RUN echo 'root:rootpw!' | chpasswd
+# RUN echo root:root | chpasswd
+#RUN echo 'root' | passwd --stdin root 
+#RUN useradd -m root 
+echo "root:root" | chpasswd 
+adduser root sudo
+#USER root
+
+################### install ssh  ####################
+#### docker : service  name commande
+apt-get update
+apt-get install ufw -y
+apt install openssh-server -y
+#service ssh enable
+ufw allow 22/tcp
+ufw allow ssh  
+service ssh start 
+#service ssh status 
+# enlever le commentaire de #Port 22
+sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+# enable root permission ssh remote
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+service ssh restart
+apt-get update 
+#RUN service ssh start
+
+
+################### remote docker  ####################
+apt-get install docker.io -y
+
+
+################### other  ####################
+apt-get install psmisc -y # enable to run killall
+apt-get install systemctl -y 
+
+apt install python3-pip -y
+apt install python3-venv -y
+apt-get install curl -y
+
+apt-get install openvpn unzip -y
+apt install git -y
+
+
+
+
+apt-get install xterm -y
+#pip install psycopg2 or 
+
+################### install package python  ####################
+pip install psycopg2-binary numpy pandas 
+
+
+
+
+
+
+
+mkdir /myapp/
+chmod -R 777 /myapp 
+
+################### clone tools_public  ####################
+rm -r /myapp/tools_public & mkdir /myapp/tools_public
+git clone https://github.com/AinaKIKISAGBE/tools_public.git /myapp/tools_public
+
+##### save github_key
+python3 /myapp/tools_public/python_sql_database/save_github_key_on_vm.py
+
+################### clone tools_private  ####################
+GITHUB_TOKEN_keep=$(< /GITHUB_TOKEN)
+rm -r /myapp/tools_private & mkdir /myapp/tools_private
+#git clone https://$GITHUB_TOKEN_keep@github.com/AinaKIKISAGBE/tools_private.git /myapp/tools_private or 
+git clone https://${GITHUB_TOKEN_keep}@github.com/AinaKIKISAGBE/tools_private.git /myapp/tools_private
+chmod -R 777 /myapp 
+
+################### service creator  master_update_iptable ####################
+chmod 777 /myapp/tools_public/tools/service_creator.sh  
+#chmod 777 /myapp/tools_public/tools/service_creator_docker.sh  
+logoutput_master_update_iptable /myapp/tools_public/tools/service_creator.sh "master_update_iptable" "/myapp/tools_private/vm_provitionning/master/master_update_iptable.sh"  & ls && ls
+
+
+################### save SURFSHARK_key   ####################
+mkdir /SURFSHARK_TOKEN/
+chmod -R 777 /SURFSHARK_TOKEN 
+python3 /myapp/tools_public/python_sql_database/save_SURFSHARK_key_on_vm.py
+
+#### add permission 
+chmod -R 777 /myapp 
+chmod +x /myapp/tools_private/vm_provitionning/vpn/surfshak/surfshak_vpn_openvpn.sh
+
+
+################### service creator  master_run_jobmaster ####################
+chmod 777 /myapp/tools_public/tools/service_creator.sh  
+logoutput_master_run_jobmaster /myapp/tools_public/tools/service_creator.sh "master_run_jobmaster" "/myapp/tools_private/vm_provitionning/master/master_run_jobmaster.sh" & ls && ls
+
+
+################### service creator  launch_worker_docker ####################
+chmod 777 /myapp/tools_public/tools/service_creator.sh  
+logoutput_launch_worker_docker /myapp/tools_public/tools/service_creator.sh "launch_worker_docker" "/myapp/tools_private/vm_provitionning/master/launch_worker_docker.sh"  & ls && ls
+
+
+
+
+
+
