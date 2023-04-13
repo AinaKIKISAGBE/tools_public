d32b485b5f7e3f02eaff66ad3aa84ead15e4939b
Amiral KIKI-SAGBE <aina.kiki.sagbe@gmail.com>
2023-04-13 05:36:34 +0200

    frfr

--- python_sql_database/connect_to_postgresql.py
+++ python_sql_database/connect_to_postgresql.py
@@ -5,14 +5,28 @@ def create_connection():
 	# Connect to the database
 	# using the psycopg2 adapter.
 	# Pass your database name ,# username , password ,
+	### keep pc_category
+	pc_category=$(cat < /pc_id/pc_category)
+	
+	if pc_category = "master" : # master
+		ip_db = "192.168.1.47"
+	elif pc_category = "worker_docker": # worker docker
+		ip_db = "10.0.0.5"
+	elif pc_category = "worker": # worker VM
+		ip_db = "192.168.1.47"	
+		
 	# hostname and port number
 	conn = psycopg2.connect(dbname='keysdb',
 							user='postgres',
 							password='postgres',
-							host='192.168.1.164',
+							host= ip_db , # '192.168.1.164',
 							port='5432')
 	# Get the cursor object from the connection object
 	curr = conn.cursor()
+	
+	
+	
+	
 	return conn, curr
 
 
