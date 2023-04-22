5a99a4690ab2885a8e093ef535aa4e1d269dc0d1
Amiral KIKI-SAGBE <aina.kiki.sagbe@gmail.com>
2023-04-22 16:19:20 +0200

    frfrg56446954454264s4o485

--- postgresql_pgAdmin/run.sh
+++ postgresql_pgAdmin/run.sh
@@ -1,1 +1,14 @@
-docker-compose up -d
+
+docker-compose -p=database_postgres up -d
+
+
+
+docker-compose up -d 
+docker-compose up -d --project-name database 
+
+docker-compose -p project_name build
+docker-compose --project-name foo build bar
+docker-compose -p app up --build
+
+docker-compose -f <docker_compose_file> -p "<project_name>" up
+docker-compose -p="stack-example" up
