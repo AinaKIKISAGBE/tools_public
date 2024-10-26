3d89ecfbadb089394f92e1c27a2f8bc950e3a2f5
Aina KIKI-SAGBE <40332540+AinaKIKISAGBE@users.noreply.github.com>
2024-10-26 12:47:45 +0200

    Create create_repos_on_gith_with_token.py

---
+++ create_repos_on_gith/create_repos_on_gith_with_token.py
@@ -0,0 +1,39 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", "-n", type=str, dest="name", required=True) # alias : -n
+parser.add_argument("--description", "-d", type=str, dest="is_description", default="nono", required=False) # alias : -d
+parser.add_argument("--private", "-p", dest="is_private", #action="store_true", 
+                    required=False, type=str , default="false" ) # alias : -p
+parser.add_argument("--github_token", "-g", type=str, dest="github_token", required=True) # alias : -g
+args = parser.parse_args()
+
+repo_name = args.name
+is_private = args.is_private
+description = args.is_description
+
+
+GITHUB_TOKEN = args.github_token
+
+
+API_URL = "https://api.github.com"
+paylod = '{"name": "%s", "private":  %s , "description": "%s"}' %(repo_name, is_private, description)
+headers = {
+    "Authorization": "token " + GITHUB_TOKEN,
+    "Accept": "application/vnd.github.v3+json",
+}
+
+try :
+    r= requests.post(API_URL + "/user/repos", data = paylod, headers = headers)
+    r.raise_for_status()
+except Exception as error :
+    raise SystemExit(error)
+
+# exemple run :
+# python create_repos_on_gith.py --name tools_private --private true --description "begin tools_private" --github_token "your github token"
+# python create_repos_on_gith.py --github_token "your github token" --name tools_public --private false --description "begin tools_public" 
+
+# avec les alias 
+# python create_repos_on_gith.py -g "your github token" -n tools_public -p false -d "begin tools_public" 
+
